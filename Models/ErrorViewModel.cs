1048e0417db64518ff57f1092d2515d8535019f8
Admin <Admin@ADMIN-PC>
2024-11-22 15:31:20 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace websitebenhvien.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
