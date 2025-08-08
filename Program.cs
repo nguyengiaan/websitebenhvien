e1834749597f321901fd47528ec77fe8f6b08fb8
Admin <Admin@ADMIN-PC>
2025-08-08 08:07:08 +0700

    góc cảm nhận 2

--- Program.cs
+++ Program.cs
@@ -52,6 +52,7 @@ builder.Services.AddScoped<EmailSender>();
 builder.Services.AddScoped<Hubnot>();
 builder.Services.AddScoped<Uploadfile>();
 builder.Services.AddScoped<IAdminmenu, AdminmenuReponsive>();
+builder.Services.AddScoped<IReceivingAngle, ReceivingAngleReponsive>();
 builder.Services.Configure<FileSystemConfig>(builder.Configuration.GetSection(FileSystemConfig.ConfigName));
 builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
 builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
